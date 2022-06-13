df6626e5d61b951b3a9831fd8d58f74ac04d8bfe
dreessen-n <dreessen.edu@gmail.com>
2022-06-13 10:54:12 -0600

    Update

--- fundamentals/oop/deck_of_cards/classes/players.py
+++ fundamentals/oop/deck_of_cards/classes/players.py
@@ -1,8 +1,14 @@
 """Create players class"""
 
+import classes.deck
+
 class Players:
     """Create a model of players"""
     def __init__(self, name):
         """Initialize the attributes"""
         self.name = name
         self.hand = []
+
+def __repr__(self):
+    display = f"{self.string_val} of {self.suit} : {self.point_val} points"
+    return display
--- fundamentals/oop/deck_of_cards/game.py
+++ fundamentals/oop/deck_of_cards/game.py
@@ -1,5 +1,7 @@
-from classes.deck import Deck
+# Import shuffle from random and classes
 from random import shuffle
+from classes.deck import Deck
+from classes.players import Players
 
 # Create Deck of cards
 bicycle = Deck()
@@ -8,11 +10,20 @@ bicycle = Deck()
 bicycle.shuffle_deck()
 
 # Print the shuffled deck
-bicycle.show_cards()
+# bicycle.show_cards()
 
-# TODO Create 2 players
+# Create computer-player
+player_comp = Players('Computer')
 
+# Create 1 players
+player_1 = Players(input("What is your name: "))
+
+# print(bicycle.show_cards())
 # TODO Distribute cards to each player
+for item in range(52):
+    if item % 2 == 0:
+        player_1.hand.append(bicycle.cards[item])
+        print(player_1.hand[item].card_info())
 
 # TODO Play game:
 # game goes until one player has all the cards
