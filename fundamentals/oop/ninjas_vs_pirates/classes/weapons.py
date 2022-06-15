e54d8b9219b311351d903a58c872b456138595fa
dreessen-n <dreessen.edu@gmail.com>
2022-06-15 14:02:04 -0600

    Update

---
+++ fundamentals/oop/ninjas_vs_pirates/classes/weapons.py
@@ -0,0 +1,12 @@
+# Create weapons class
+
+class Weapons:
+    """Model weapons for battle use"""
+    # Make a list of all weapons
+    all_weapons = []
+
+    def __init__(self):
+        self.name = name
+        self.weapon_type = weapon_type
+        self.health_damage_pts = health_damage_pts
+        self.strength_damage_pts = strength_damage_pts
